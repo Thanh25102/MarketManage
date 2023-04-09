1d66f8bdaf0b0093071ea1ac8401b53f33b78c87
popudev <phuthinh53.it@gmail.com>
2023-04-09 16:12:04 +0700

    done gui

--- src/main/java/org/nam16tuoimatem/Main.java
+++ src/main/java/org/nam16tuoimatem/Main.java
@@ -2,19 +2,15 @@ package org.nam16tuoimatem;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.nam16tuoimatem.services.CategoryService;
-import org.nam16tuoimatem.services.CustomerService;
-import org.nam16tuoimatem.services.VegetableService;
-
-import java.util.List;
+import org.nam16tuoimatem.gui.MainGUI;
 
 
 public class Main {
     private static final Logger LOG = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        VegetableService vegetableService = VegetableService.getInstance();
-        System.out.println(vegetableService.totalMoney());
+        MainGUI main = new MainGUI();
+        main.setVisible(true);
     }
 
 }
