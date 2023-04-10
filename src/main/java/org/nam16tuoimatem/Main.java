6b9e8637d10563d7fe892b236bf90f18c8af4c85
Bùi Mạnh Thành TTS <manhthanh147@gmail.com>
2023-04-10 11:37:47 +0700

    merge migrate

--- src/main/java/org/nam16tuoimatem/Main.java
+++ src/main/java/org/nam16tuoimatem/Main.java
@@ -2,21 +2,15 @@ package org.nam16tuoimatem;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.nam16tuoimatem.exception.DateToInValidException;
-import org.nam16tuoimatem.model.SearchMap;
-import org.nam16tuoimatem.services.OrderService;
-import org.nam16tuoimatem.services.VegetableService;
+import org.nam16tuoimatem.gui.MainGUI;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
 
 
 public class Main {
     private static final Logger LOG = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        VegetableService vegetableService = VegetableService.getInstance();
-        vegetableService.findAll().forEach(System.out::println);
+        MainGUI main = new MainGUI();
+        main.setVisible(true);
     }
 }
