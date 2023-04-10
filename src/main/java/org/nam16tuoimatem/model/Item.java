ae903b4fbb28335386e5bf20e0e8cce1ee4db23e
popudev <phuthinh53.it@gmail.com>
2023-04-10 13:46:30 +0700

    bug tu lu

---
+++ src/main/java/org/nam16tuoimatem/model/Item.java
@@ -0,0 +1,27 @@
+package org.nam16tuoimatem.model;
+
+public class Item {
+    private Integer id;
+    private String value;
+
+    public Item(Integer id, String value) {
+        this.id = id;
+        this.value = value;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
