d6051f03e883d44c26d83890337cbd85a1ba844e
LuongThinh123 <luongchithinh123@gmail.com>
2023-04-14 00:03:48 +0700

    chi thinh pull

--- src/main/java/org/nam16tuoimatem/Record.java
+++ src/main/java/org/nam16tuoimatem/Record.java
@@ -1,5 +1,19 @@
 package org.nam16tuoimatem;
 
+import java.util.Date;
+
 public class Record {
-     public record VegetableRecord(Integer vegetableId,String vegetableName,String unit,Integer amount,String image,Double price, String categoryName){}
+    public record VegetableRecord(Integer vegetableId,String vegetableName,String unit,Integer amount,String image,Double price, String categoryName){}
+
+    public record CategoryRecord(Integer categoryId, String name, String description) {
+    }
+
+    public record CustomerRecord(Integer customerId, String password, String fullname, String address, String city) {
+    }
+
+    public record VegetableOrderRecord(Integer vegetableId, String vegetableName, Double price, Integer count, Double total) {
+    }
+
+    public record OrderDetailRecord(Integer orderId, Integer vegetableId, Integer quantity, Double price) {
+    }
 }
